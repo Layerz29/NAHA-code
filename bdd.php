fa55edf510496a435cacb2ce3ade5c078547b660
Arth <arthur.feschet@etu.univ-montp3.fr>
2025-11-26 16:27:58 +0100

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 function getBD() {
   try{
-    //si vous arrivez pas à vous connecter, vérifier le bon port
+    //si vous arrivez pas à vous connecter, vérifier le bon port ahmed tu peux repush ton code de base ? j'ai push le mien sans faire exprès
     $dsn = "mysql:host=localhost;port=3306;dbname=naha;charset=utf8";
     $user = "root";
     $pass = "root";  
